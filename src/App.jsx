8d861e024fd164826ee2fdc895b3c5adebcd5daf
arthur2009ak-rgb <arthur2009ak@gmail.com>
2026-09-02 12:10:57 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import Hero from './components/Hero'
-import Intro from './components/Intro'
-import Updates from './components/Updates'
-import Ending from './components/Ending'
-import Footer from './components/Footer'
+import Hero from './compenents/Hero'
+import Intro from './compenents/Intro'
+import Updates from './compenents/Updates'
+import Ending from './compenents/Ending'
+import Footer from './compenents/Footer'
 
 export default function App() {
   return (
